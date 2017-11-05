730b8ef823dce20434bef6019cef628fd96e6b32
596142041 <wcj53636746@126.com>
2017-11-05 13:29:51 +0800

    LED配置

---
+++ bsp/inc/LED.h
@@ -0,0 +1,16 @@
+/*
+ * LED.h
+ *
+ *  Created on: 2017��5��21��
+ *      Author: admin
+ */
+#ifndef BSP_INC_LED_H_
+#define BSP_INC_LED_H_
+#include "DSP2833x_Device.h"
+#include "DSP2833x_Examples.h"
+#include "data.h"
+#include "delay.h"
+#include "stdint.h"
+void LED_GPIO_Config(void);
+void LED_Timer_Config(void);
+#endif /* BSP_INC_LED_H_ */
---
+++ bsp/src/LED.c
@@ -0,0 +1,38 @@
+/*
+ * LED.c
+ *
+ *  Created on: 2017��5��21��
+ *      Author: admin
+ */
+#include "LED.h"
+void LED_GPIO_Config(void)
+{
+	   EALLOW;
+		   GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+		   GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 0;
+		   GpioCtrlRegs.GPBMUX2.bit.GPIO58 = 0;
+		   GpioCtrlRegs.GPBMUX2.bit.GPIO59 = 0x00;
+		   GpioCtrlRegs.GPBMUX2.bit.GPIO60 = 0;
+		   GpioCtrlRegs.GPBMUX2.bit.GPIO61 = 0;
+		   GpioCtrlRegs.GPBDIR.bit.GPIO58 = 1;
+		   GpioCtrlRegs.GPBDIR.bit.GPIO59 = 1;
+		   GpioCtrlRegs.GPBDIR.bit.GPIO60 = 1;
+		   GpioCtrlRegs.GPBDIR.bit.GPIO61 = 1;
+		   GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+		   GpioCtrlRegs.GPADIR.bit.GPIO0 = 1;
+	   EDIS;
+	   GpioDataRegs.GPBCLEAR.all = 0xFFFFFFFF;
+	   GpioDataRegs.GPASET.bit.GPIO0 = 1;
+}
+void LED_Timer_Config(void)
+{
+	EALLOW;
+		SysCtrlRegs.PCLKCR3.bit.CPUTIMER0ENCLK = 1;
+	EDIS;
+	InitCpuTimers();
+	ConfigCpuTimer(&CpuTimer0, 150, 200000);
+	CpuTimer0Regs.TCR.all = 0x4001; // Use write-only instruction to set TSS bit = 0
+
+}
+
+
